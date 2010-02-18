88774a6aa8820fc99c5d87e07ec0953a4559370a
Tammer Saleh <tsaleh@gmail.com>
2010-02-18 15:26:27 -0800

    Updated styles and added thumbnails

--- podium.gemspec
+++ podium.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{podium}
-  s.version = "0.1.1"
+  s.version = "0.1.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["tsaleh"]
-  s.date = %q{2010-02-10}
+  s.date = %q{2010-02-18}
   s.description = %q{HAML and JS based presentation system.}
   s.email = %q{tsaleh@gmail.com}
   s.executables = ["podium", "showoff2podium"]
--- templates/javascript/slides.js
+++ templates/javascript/slides.js
@@ -1,36 +1,67 @@
 $(function() {
-  var hash = window.location.hash;
-  var number = 0;
-  if (hash != "") { number = hash.split("_")[1]; }
-  focusSlide($(".slide").eq(number));
+  setupSlideThumbnails();
+  focusFirstSlide();
   document.onkeydown = keyDown;
 });
 
+function setupSlideThumbnails() {
+  $('<div id="thumbnails"></div>').html($("#slides").html()).appendTo("body");
+  $('#thumbnails .slide').hover(function() { $(this).addClass("hover")   }, 
+                                      function() { $(this).removeClass("hover")});
+  $('#thumbnails .slide').click(function() { 
+    focusSlide($("#slides .slide").eq($("#thumbnails .slide").index($(this))));
+    toggleThumbs();
+  });
+  toggleThumbs();
+};
+
+function toggleThumbs() {
+  if ($('#thumbnails').is(':visible')) {
+    $('#thumbnails').hide();
+    $('#slides').show();
+  } else {
+    $('#thumbnails').show();
+    $('#slides').hide();
+  }
+};
+
+function focusFirstSlide() {
+  var hash = window.location.hash;
+  var number = 0;
+  if (hash != "") { number = hash.split("_")[1]; }
+  focusSlide($("#slides .slide").eq(number));
+}
+
 function focusSlide($slide) {
   if ($slide.first().hasClass("slide")) {
-    window.location.hash = "#slide_" + $(".slide").index($slide);
-    $(".slide").removeClass("current_slide");
-    $slide.addClass("current_slide");
+    window.location.hash = "#slide_" + $("#slides .slide").index($slide);
+    $("#slides .slide").removeClass("current");
+    $slide.addClass("current");
+
+    $thumb = $("#thumbnails .slide").eq($("#slides .slide").index($slide))
+    $("#thumbnails .slide").removeClass("current");
+    $thumb.addClass("current");
     centerSlide();
   };
 };
 
 function next() {
-  if ($(".current_slide .inc:hidden").length > 0) {
-    $(".current_slide .inc:hidden").first().show();
+  if ($("#slides .current .inc:hidden").length > 0) {
+    $("#slides .current .inc:hidden").first().show();
   } else {
-    focusSlide($(".current_slide").next());
+    focusSlide($("#slides .current").next());
   }
 }
 
 function prev() {
-  focusSlide($(".current_slide").prev());
+  focusSlide($("#slides .current").prev());
 }
 
 function centerSlide() {
-  var slide_height = $(".current_slide").height()
-  var mar_top = (0.5 * parseFloat($("#slides").height())) - (0.5 * parseFloat(slide_height))
-  $(".current_slide").css('margin-top', mar_top)
+  var slide_height = $("#slides .current").height()
+  var top = (0.5 * parseFloat($("#slides").height())) - (0.5 * parseFloat(slide_height))
+  $("#slides .current").css('padding-top', top)
+  $("#slides .current").css('padding-bottom', top)
 }
 
 //  See e.g. http://www.quirksmode.org/js/events/keys.html for keycodes
@@ -45,8 +76,12 @@ function keyDown(event)
 
   if (key == 32 || key == 39 || key == 40) { 
     next();
+  } else if (key == 67) { 
+    toggleThumbs();
   } else if (key == 37 || key == 38) { 
     prev();
+  } else {
+    return true;
   }
   return false 
 }
